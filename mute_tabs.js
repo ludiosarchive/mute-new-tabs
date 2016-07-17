d9db36a9c49a8b9d665dad27b72d3e5432e197bb
Ivan Kozik <ivan@ludios.org>
2016-07-17 03:16:52 +0000

    Add option to mute tab when origin changes

--- mute_tabs.js
+++ mute_tabs.js
@@ -1,16 +1,57 @@
-function maybeMuteTab(tab) {
+// TODO: listen for storage events instead of getting the setting each time?
+
+function orTrue(v) {
+	return v === undefined ? true : v;
+}
+
+function maybeMuteNewTab(tab) {
 	chrome.storage.local.get('muteNewTabs', function(result) {
-		const muteNewTabs =
-			result.muteNewTabs === undefined ?
-				true :
-				result.muteNewTabs;
+		const muteNewTabs = orTrue(result.muteNewTabs);
+		const tabId = tab.id;
 		if(muteNewTabs) {
-			console.log("Muting tab:", tab);
-			chrome.tabs.update(tab.id, {muted: true});
+			console.log("Muting new tab:", tab);
+			chrome.tabs.update(tabId, {muted: true});
 		} else {
-			console.log("Not muting tab:", tab);
+			console.log("Not muting new tab:", tab);
 		}
 	});
 }
 
-chrome.tabs.onCreated.addListener(maybeMuteTab);
+function getOrigin(url) {
+	return new URL(url).origin;
+}
+
+// TODO: remove entries when tabs are closed
+const tabIdToUrl = Object.create(null);
+
+function navigationCommitted(details) {
+	if(details.frameId !== 0) {
+		// Ignore navigations in subframes
+		return;
+	}
+	const tabId = details.tabId;
+	const newUrl = details.url;
+	chrome.storage.local.get('muteOnOriginChange', function(result) {
+		const muteOnOriginChange = orTrue(result.muteOnOriginChange);
+		if(muteOnOriginChange) {
+			const oldUrl = tabIdToUrl[tabId];
+			console.log("Old URL:", oldUrl);
+			let needMute = false;
+			if(oldUrl === undefined) {
+				needMute = true;
+			} else {
+				needMute = getOrigin(oldUrl) !== getOrigin(newUrl);
+			}
+			if(needMute) {
+				console.log("Muting tab because new origin:", details);
+				chrome.tabs.update(tabId, {muted: true});
+			} else {
+				console.log("Not muting tab because same origin:", details);
+			}
+		}
+		tabIdToUrl[tabId] = newUrl;
+	});
+}
+
+chrome.webNavigation.onCommitted.addListener(navigationCommitted);
+chrome.tabs.onCreated.addListener(maybeMuteNewTab);
--- popup.js
+++ popup.js
@@ -1,14 +1,24 @@
 const muteNewTabsElem = document.getElementById('mute_new_tabs');
+const muteOnOriginChange = document.getElementById('mute_on_origin_change');
 
 muteNewTabsElem.onchange = function(ev) {
-	const mute = ev.target.checked;
-	chrome.storage.local.set({muteNewTabs: mute}, function() {});
+	chrome.storage.local.set({muteNewTabs: ev.target.checked}, function() {});
+}
+
+muteOnOriginChange.onchange = function(ev) {
+	chrome.storage.local.set({muteOnOriginChange: ev.target.checked}, function() {});
+}
+
+function orTrue(v) {
+	return v === undefined ? true : v;
 }
 
 chrome.storage.local.get('muteNewTabs', function(result) {
-	const muteNewTabs =
-		result.muteNewTabs === undefined ?
-			true :
-			result.muteNewTabs;
+	const muteNewTabs = orTrue(result.muteNewTabs);
 	muteNewTabsElem.checked = muteNewTabs;
+});
+
+chrome.storage.local.get('muteOnOriginChange', function(result) {
+	const muteOnOriginChange = orTrue(result.muteOnOriginChange);
+	muteOnOriginChange.checked = muteOnOriginChange;
 });
