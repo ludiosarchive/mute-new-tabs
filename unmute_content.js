5aff1c99698b0aba103ac2423e2690db22169b1c
Ivan Kozik <ivan@ludios.org>
2017-03-05 23:29:55 +0000

    Refactor mouseDown volume control checks; update automatic unmute for www.cnn.com; add automatic unmute for edition.cnn.com

--- unmute_content.js
+++ unmute_content.js
@@ -24,8 +24,6 @@ function getClassNames(elem, depth) {
 	return classNames;
 }
 
-const host = document.location.host;
-
 function includesAny(haystack, needles) {
 	for(const needle of needles) {
 		if(haystack.includes(needle)) {
@@ -35,43 +33,28 @@ function includesAny(haystack, needles) {
 	return false;
 }
 
+const volumeControlChecks = {
+	 "www.youtube.com":        ev => /^ytp-(volume|mute)-/.test(ev.target.className)
+	,"www.liveleak.com":       ev => getClassNames(ev.target, 6).includes("vjs-volume-menu-button")
+	,"vimeo.com":              ev => getClassNames(ev.target, 3).includes("volume")
+	,"twitter.com":            ev => getClassNames(ev.target, 4).includes("volume-control-container")
+	,"vine.co":                ev => includesAny(getClassNames(ev.target, 3), ["VolumeControl", "vine-audio"])
+	,"soundcloud.com":         ev => /^volume__/.test(ev.target.className)
+	,"www.twitch.tv":          ev => getClassNames(ev.target, 4).includes("player-volume")
+	,"player.twitch.tv":       ev => getClassNames(ev.target, 4).includes("player-volume")
+	,"mynoise.net":            ev => includesAny(getClassNames(ev.target, 5), ["mixer", "controlers"])
+	,"www.kickstarter.com":    ev => includesAny(getClassNames(ev.target, 4), ["volume", "volume_container"])
+	,"store.steampowered.com": ev => includesAny(getClassNames(ev.target, 3), ["volume_icon", "volume_slider"])
+	,"vid.me":                 ev => includesAny(getClassNames(ev.target, 4), ["vjs-volume-menu-button", "vjs-volume-bar"])
+	,"www.cnn.com":            ev => includesAny(getClassNames(ev.target, 4), ["vjs-volume-control", "vjs-mute-control"])
+	,"edition.cnn.com":        ev => includesAny(getClassNames(ev.target, 4), ["vjs-volume-control", "vjs-mute-control"])
+};
+
+const host = document.location.host;
+
 function mouseDown(ev) {
-	if(host === "www.youtube.com" && /^ytp-(volume|mute)-/.test(ev.target.className)) {
+	if(volumeControlChecks[host](ev)) {
 		unmuteMyTab();
-	} else if(host === "www.liveleak.com" && getClassNames(ev.target, 6).includes("vjs-volume-menu-button")) {
-		unmuteMyTab();
-	} else if(host === "vimeo.com" && getClassNames(ev.target, 3).includes("volume")) {
-		unmuteMyTab();
-	} else if(host === "twitter.com" && getClassNames(ev.target, 4).includes("volume-control-container")) {
-		unmuteMyTab();
-	} else if(host === "vine.co" && includesAny(getClassNames(ev.target, 3), ["VolumeControl", "vine-audio"])) {
-		unmuteMyTab();
-	} else if(host === "soundcloud.com" && /^volume__/.test(ev.target.className)) {
-		unmuteMyTab();
-	} else if(host === "www.twitch.tv" && getClassNames(ev.target, 4).includes("player-volume")) {
-		unmuteMyTab();
-	} else if(host === "player.twitch.tv" && getClassNames(ev.target, 4).includes("player-volume")) {
-		unmuteMyTab();
-	} else if(host === "mynoise.net") {
-		const classNames = getClassNames(ev.target, 5);
-		if(classNames.includes("mixer") || classNames.includes("controlers")) {
-			unmuteMyTab();
-		}
-	} else if(host === "www.kickstarter.com") {
-		const classNames = getClassNames(ev.target, 4);
-		if(classNames.includes("volume") || classNames.includes("volume_container")) {
-			unmuteMyTab();
-		}
-	} else if(host === "store.steampowered.com") {
-		const classNames = getClassNames(ev.target, 3);
-		if(classNames.includes("volume_icon") || classNames.includes("volume_slider")) {
-			unmuteMyTab();
-		}
-	} else if(host === "vid.me") {
-		const classNames = getClassNames(ev.target, 4);
-		if(classNames.includes("vjs-volume-menu-button") || classNames.includes("vjs-volume-bar")) {
-			unmuteMyTab();
-		}
 	}
 }
 
@@ -79,27 +62,3 @@ function mouseDown(ev) {
 // 1) the elements sometimes show up after the DOM is ready
 // 2) the elements may be swapped out when switching to another video
 document.addEventListener('mousedown', mouseDown, {passive: true});
-
-if(host === "www.cnn.com") {
-	const seen = new Set();
-	// cnn.com uses <video> elements with controls="true".  The volume controls
-	// are part of the Shadow DOM, and click events on the controls don't result
-	// in any kind of click event that we can detect.   We need to use another
-	// approach to detect intentional use of the volume control, so we use the
-	// 'volumechange' event.
-	//
-	// We assume here that only user interaction leads to a volumechange event,
-	// so if the page violates this expectation in the future, this has to be
-	// reworked to ignore some volumechange events.
-	//
-	// The <video> elements aren't on the page when this script is first run,
-	// so check for new <video>s often.
-	setInterval(() => {
-		for(const video of document.getElementsByTagName('video')) {
-			if(!seen.has(video)) {
-				video.addEventListener('volumechange', unmuteMyTab, false);
-				seen.add(video);
-			}
-		}
-	}, 2000);
-}
